d1d780b7f0e7d3d20f776df1cbc151971c07436b
Grehivin <grehivin.vargas@uamcr.net>
2022-11-12 15:33:50 -0600

    Add project files.

---
+++ RestrictedUI/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace RestrictedUI.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
