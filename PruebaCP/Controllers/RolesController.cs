c87329721d4c0de49edfda827c8b8cfdad88d845
Grehivin <grehivin.vargas@uamcr.net>
2022-11-16 20:16:29 -0600

    Creación de PL de prueba.

---
+++ PruebaCP/Controllers/RolesController.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using AccesoDatos;
+using Entidades;
+
+namespace PruebaCP.Controllers
+{
+    public class RolesController : Controller
+    {
+        private readonly HotelContext _context;
+
+        public RolesController()//(HotelContext context)
+        {
+            _context = new HotelContext(); //context;
+        }
+
+        // GET: Roles
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Roles.ToListAsync());
+        }
+
+        // GET: Roles/Details/5
+        public async Task<IActionResult> Details(decimal? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var roles = await _context.Roles
+                .FirstOrDefaultAsync(m => m.IdRol == id);
+            if (roles == null)
+            {
+                return NotFound();
+            }
+
+            return View(roles);
+        }
+
+        // GET: Roles/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Roles/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("IdRol,Descripcion,RolActivo")] Roles roles)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(roles);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(roles);
+        }
+
+        // GET: Roles/Edit/5
+        public async Task<IActionResult> Edit(decimal? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var roles = await _context.Roles.FindAsync(id);
+            if (roles == null)
+            {
+                return NotFound();
+            }
+            return View(roles);
+        }
+
+        // POST: Roles/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(decimal id, [Bind("IdRol,Descripcion,RolActivo")] Roles roles)
+        {
+            if (id != roles.IdRol)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(roles);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!RolesExists(roles.IdRol))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(roles);
+        }
+
+        // GET: Roles/Delete/5
+        public async Task<IActionResult> Delete(decimal? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var roles = await _context.Roles
+                .FirstOrDefaultAsync(m => m.IdRol == id);
+            if (roles == null)
+            {
+                return NotFound();
+            }
+
+            return View(roles);
+        }
+
+        // POST: Roles/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(decimal id)
+        {
+            var roles = await _context.Roles.FindAsync(id);
+            _context.Roles.Remove(roles);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool RolesExists(decimal id)
+        {
+            return _context.Roles.Any(e => e.IdRol == id);
+        }
+    }
+}
